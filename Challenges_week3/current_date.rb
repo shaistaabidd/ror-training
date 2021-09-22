9fdfb5dabfdc8da073ac25c1eb53d1cf7f1da8d7
shaistaabidd <shaistaabid76@gmail.com>
2021-09-22 10:52:50 +0500

    display current date and time

--- Challenges_week3/current_date.rb
+++ Challenges_week3/current_date.rb
@@ -2,9 +2,7 @@
 #Write a Ruby program to display the current date and time.
 
 #Sample Output:
-
 #Current Date and Time: 27/12/2017 06:04
-
 require 'date'
 date_time = DateTime.now
 date_now=date_time.strftime "%d/%m/%Y"
