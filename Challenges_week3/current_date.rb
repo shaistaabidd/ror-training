b3dc499964a5130b3b4aff8e608783278f975e52
shaistaabidd <shaistaabid76@gmail.com>
2021-09-22 10:51:04 +0500

    current_date.rb

--- Challenges_week3/current_date.rb
+++ Challenges_week3/current_date.rb
@@ -6,6 +6,9 @@
 #Current Date and Time: 27/12/2017 06:04
 
 require 'date'
-current_time = DateTime.now
-cdt = current_time.strftime "%d/%m/%Y %H:%M"
-puts "Current Date and Time: "+cdt
+date_time = DateTime.now
+date_now=date_time.strftime "%d/%m/%Y"
+time_now=date_time.strftime " %H:%M"
+#cdt = current_time.strftime "%d/%m/%Y %H:%M"
+puts "Current Date: " +date_now
+puts "Current Time : "+time_now
