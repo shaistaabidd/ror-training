95bd9766a3b9a9f60b603aa5f87ea37588cbee2c
shaistaabidd <shaistaabid76@gmail.com>
2021-09-22 11:23:13 +0500

    returns how many Friday 13ths there are in a given year

--- Challenges_week3/friday_thirteen.rb
+++ Challenges_week3/friday_thirteen.rb
@@ -3,9 +3,8 @@
 #how_unlucky(2020) ➞ 2
 #how_unlucky(2026) ➞ 3
 #how_unlucky(2016) ➞ 1
-
+require 'date'
 def how_unlucky(year)
-    require 'date'
     (1..12).count { |month| Date.new(year, month, 13).friday? }
 end
 print how_unlucky(2016) 
