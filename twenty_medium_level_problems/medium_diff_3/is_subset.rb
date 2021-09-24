d6751ec1dc67a9d94fd9b7800351d6babe421fa0
shaistaabidd <shaistaabid76@gmail.com>
2021-09-24 16:58:42 +0500

    check if first array is the subset of second

--- twenty_medium_level_problems/medium_diff_3/is_subset.rb
+++ twenty_medium_level_problems/medium_diff_3/is_subset.rb
@@ -1,3 +1,9 @@
+#Create a function that returns true if the first array is a subset of the second. Return false otherwise.
+#Examples
+#is_subset([3, 2, 5], [5, 3, 7, 9, 2])   true➞
+#is_subset([8, 9], [7, 1, 9, 8, 4, 5, 6])   true➞
+#is_subset([1, 2], [3, 5, 9, 1])   false
+
 def is_subset(arr1, arr2)
   (arr1 - arr2).empty?
 end
